d6844546618cf3434824b24e8345580006715c04
SupImDos <SupImDos@gmail.com>
2024-05-09 22:49:42 +0800

    Add dummy untested files to test coverage functionality.

---
+++ src/pydantic_argparse/test/test.py
@@ -0,0 +1,9 @@
+"""This is a test file."""
+
+def test_function() -> None:
+    """This is a test function."""
+    assert 1 + 1 == 2
+    assert 2 + 2 == 4
+    assert 3 + 3 == 6
+    assert 4 + 4 == 8
+    assert 5 + 5 == 10
